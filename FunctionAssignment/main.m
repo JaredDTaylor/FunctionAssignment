5835f80142db0ef9a8c8239ae36fd39c3a20d9dd
Jared <jaredtaylor@cbrc.net>
2015-04-13 10:45:51 -0400

    added functions

--- FunctionAssignment/main.m
+++ FunctionAssignment/main.m
@@ -7,14 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+bool IsOdd(int i){
+    if (i%2 != 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+bool IsEven(int i){
+    if (i%2 == 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
         int i;
         for (i = 0; i <= 1000; i++){
-            if (i%2 == 0){
+            if (IsEven(i)){
                 NSLog(@"Even");
-            }else{
+            }else if (IsOdd(i)){
                 NSLog(@"Odd");
             }
         }
